9ff5def584a5f34ba631307557a8b2aec287c14c
Sebas0320 <141646962+Sebas0320@users.noreply.github.com>
2025-04-22 10:27:27 -0600

    Update Burbuja.java

--- Burbuja.java
+++ Burbuja.java
@@ -1,1 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author rodse
+ */
+public class IdentificarExpresiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String curso = "Lenguajes Orientados a Objetos";
+        String numero = "123.45";
+        String palabra;
+        int entero;
+        System.out.println(curso.charAt(4));
+        System.out.println(curso.length());
+        palabra=curso.substring(23).toUpperCase();
+        System.out.println(palabra);
+        entero=Integer.parseInt(numero.substring(0,numero.indexOf(".")));
+        System.out.println(entero);
+    }
+}
 
